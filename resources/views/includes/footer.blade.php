b010e306e11767e26f784366623bb06aaa27f02e
LisandroGuerra <lisandro.digital@gmail.com>
2020-08-21 21:47:58 -0300

    Footer colors and popover adjusts

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,33 +1,40 @@
 <style>
     footer {
         background-color: #002133;
-        /*background-color: #00314D;*/
     }
     .nav button.btn-footer {
         box-shadow: none;
         border: none;
         border-radius: 0;
-        background-color: #1A2A37;
+        background-color: #34495E;
         color: azure;
         font-size: 0.8rem;
         transition: 0.4s;
     }
     .nav button.btn-footer:hover {
-        background-color: #002133;
+        background-color: #23527C;
     }
     .navbar-text {
         color: azure;
         font-size: 0.6rem;
+    }
+    .popover-header {
+        background-color: #23527C;
+        color: #FFF;
     }
 </style>
 
 <footer class="footer fixed-bottom">
     <div class="nav d-flex justify-content-between align-items-center">
         <button type="button" class="btn btn-info btn-footer"
+                title="Informações"
+                data-html="true"
                 data-container="body"
                 data-toggle="popover"
                 data-placement="top"
-                data-content="Informações">
+                data-content="<p class='font-info'>
+                <b>Desenvolvedor:</b> Lisandro Guerra<br/>
+                <b>Versão:</b> 0.8.1</p>">
             <i class="fa fa-info-circle"></i>
             Info
         </button>
@@ -38,6 +45,8 @@
 
 
         <button type="button" class="btn btn-info btn-footer"
+                title="Ajuda"
+                data-html="true"
                 data-container="body"
                 data-toggle="popover"
                 data-placement="top"
