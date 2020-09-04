b1da8b247002c4ef1e172cd06c21e81bf0b3cd4b
LisandroGuerra <lisandro.digital@gmail.com>
2020-09-04 01:59:17 -0300

    Correting migrations reports nullable date

--- database/migrations/2020_09_04_004905_create_reports_table.php
+++ database/migrations/2020_09_04_004905_create_reports_table.php
@@ -20,7 +20,7 @@ class CreateReportsTable extends Migration
             $table->string('program');
             $table->string('designation_act');
             $table->date('start_date');
-            $table->date('end_date');
+            $table->date('end_date')->nullable();
             //$table->timestamps();
         });
     }
