0a897c2ee95609212ddb51816a819f36dbed2fe5
LisandroGuerra <lisandro.digital@gmail.com>
2020-09-04 01:23:58 -0300

    MCR for reports table

---
+++ database/migrations/2020_09_04_004905_create_reports_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('registry');
+            $table->string('analyst');
+            $table->string('program');
+            $table->string('designation_act');
+            $table->date('start_date');
+            $table->date('end_date');
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
