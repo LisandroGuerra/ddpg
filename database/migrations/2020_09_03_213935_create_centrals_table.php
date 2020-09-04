c1ec4c0dae17dabdbc599dd7500b450b347ea657
LisandroGuerra <lisandro.digital@gmail.com>
2020-09-03 21:41:15 -0300

    Migration centrals table corretion

--- database/migrations/2020_09_03_213935_create_centrals_table.php
+++ database/migrations/2020_09_03_213935_create_centrals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableCentrals extends Migration
+class CreateCentralsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTableCentrals extends Migration
      */
     public function up()
     {
-        Schema::create('table_centrals', function (Blueprint $table) {
+        Schema::create('centrals', function (Blueprint $table) {
             $table->increments('id')->autoIncrement();
             $table->string('central');
             $table->boolean('active')->default(true);
@@ -29,6 +29,6 @@ class CreateTableCentrals extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_centrals');
+        Schema::dropIfExists('centrals');
     }
 }
